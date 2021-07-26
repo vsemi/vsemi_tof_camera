04efbf897330ae52e11d6802f99df2cdb57caf49
vsemi <horace.li@vsemi.io>
2021-07-26 14:11:38 -0400

    added sensor ID to the title for distance.py

--- distance.py
+++ distance.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+import time
 
 from TauLidarCommon.frame import FrameType
 from TauLidarCamera.camera import Camera
@@ -9,9 +10,23 @@ def setup():
     ports = Camera.scan()                      ## Scan for available Tau Camera devices
 
     if len(ports) > 0:
+        '''
+        cameraNum = 0
+        cameraSelected = 0
+        if len(ports) >= 1:
+            print("\nMore than 1 cameras found:")
+            for port in ports:
+                print(cameraNum, ':', port)
+                cameraNum += 1
+            
+            cameraSelected = int(input("Enter the number to select a camera from the list:"))
+            print(cameraSelected, "entered, try to connect to port ", ports[cameraSelected])
+        '''
+    
         Camera.setRange(0, 4500)                   ## points in the distance range to be colored
 
-        camera = Camera.open(ports[0])             ## Open the first available Tau Camera
+        camera = Camera.open(ports[0])
+        #camera = Camera.open(ports[cameraSelected])
         camera.setModulationChannel(0)             ## autoChannelEnabled: 0, channel: 0
         camera.setIntegrationTime3d(0, 1000)       ## set integration time 0: 1000
         camera.setMinimalAmplitude(0, 10)          ## set minimal amplitude 0: 80
@@ -31,6 +46,10 @@ def setup():
 
 
 def run(camera):
+    start_t = time.time()
+    frame_count = 0
+    cameraInfo = camera.info()
+    uid = cameraInfo.uid
     while True:
         frame = camera.readFrame(FrameType.DISTANCE)
 
@@ -42,9 +61,14 @@ def run(camera):
             upscale = 4
             img =  cv2.resize(mat_depth_rgb, (frame.width*upscale, frame.height*upscale))
 
-            cv2.imshow('Depth Map', img)
+            cv2.imshow('Depth Map - ' + uid, img)
+            frame_count += 1
 
             if cv2.waitKey(1) == 27: break
+
+    end_t = time.time()
+    frame_rate = frame_count / (end_t - start_t)
+    print('\nFrame rate: ', frame_rate)
 
 
 def cleanup(camera):
